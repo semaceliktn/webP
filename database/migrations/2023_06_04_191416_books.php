b4669f9e0da2bd5af32e97fc7be13cbc8f4a2406
beyzaytmr <101430436+beyzaytmr@users.noreply.github.com>
2023-06-04 22:30:35 +0300

    commit and push

---
+++ database/migrations/2023_06_04_191416_books.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books',function (Blueprint $table){
+            $table->id('book_id');
+            $table->integer('author_id')->nullable();
+            $table->integer('publısher_id')->nullable();
+            $table->string('title')->nullable();
+            $table->string('ısbn')->nullable();
+            $table->string('genre')->nullable();
+            $table->date('publication_year')->nullable();
+            $table->double('price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
