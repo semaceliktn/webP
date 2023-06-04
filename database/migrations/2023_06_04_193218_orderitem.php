ba3f1662f09543fa33a6a712bc1f1a0b210629f7
beyzaytmr <101430436+beyzaytmr@users.noreply.github.com>
2023-06-04 23:31:20 +0300

    commit and push

---
+++ database/migrations/2023_06_04_193218_orderitem.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orderitem',function (Blueprint $table){
+            $table->id('order_id');
+            $table->unsignedBigInteger('book_id')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->double('price')->nullable();
+            $table->foreign('order_id')->references('order_id')->on('orders');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
